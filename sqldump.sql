ec68dfd18be7a041925287a2f25837359dfa44cf
timothythlau@bitbucket.org <tim@tim-ThinkPad-X120e.(none)>
2013-04-04 12:47:27 -0400

    Added admin and user type to login Added type parameter to session

--- sqldump.sql
+++ sqldump.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.29, for Linux (i686)
+-- MySQL dump 10.13  Distrib 5.5.29, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: ece458
 -- ------------------------------------------------------
--- Server version	5.5.29
+-- Server version	5.5.29-0ubuntu0.12.10.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -27,9 +27,20 @@ CREATE TABLE `Users` (
   `email` varchar(128) DEFAULT NULL,
   `password` binary(60) DEFAULT NULL,
   `secret` binary(16) DEFAULT NULL,
+  `type` enum('admin','user') DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES (25,'testuser10','$2a$12$rD5wzWucFDw17wz8AImqseliI36ZbjwpvEp/MjtLMssxOGR0Dfhgq','JFKUYNKYIFAVGVSL',NULL),(26,'testuser','$2a$12$ypinmQ4Fe6gVwQdylhOPyuqUSbeES2HXCxXi6CR63BIaGOxv/BINW','JRCVQV2SKIZVCSKY',NULL),(27,'admintest','$2a$12$GPHLGyT.FXXwfW5sBjjme.EazvjzUFXF.wdIsn3CP6AvvG5P61C52','IU2UCQKLKY3TARCC','admin');
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `options`
@@ -44,8 +55,18 @@ CREATE TABLE `options` (
   `num` int(11) NOT NULL,
   `text` varchar(128) NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `options`
+--
+
+LOCK TABLES `options` WRITE;
+/*!40000 ALTER TABLE `options` DISABLE KEYS */;
+INSERT INTO `options` VALUES (1,1,1,'option1'),(1,2,2,'option2'),(1,3,3,'option3'),(2,4,1,'option1'),(2,5,2,'option2'),(2,6,3,'option3');
+/*!40000 ALTER TABLE `options` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `polls`
@@ -62,8 +83,18 @@ CREATE TABLE `polls` (
   `endDate` datetime DEFAULT NULL,
   `salt` varchar(16) NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `polls`
+--
+
+LOCK TABLES `polls` WRITE;
+/*!40000 ALTER TABLE `polls` DISABLE KEYS */;
+INSERT INTO `polls` VALUES (1,'testPoll',NULL,'2013-04-04 12:12:02','2013-04-04 12:12:02','b69ee2dfec5bee0d'),(2,'testPoll2',NULL,'2013-04-04 12:12:02','2013-04-04 12:12:02','acbc63413b1e11d3');
+/*!40000 ALTER TABLE `polls` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `votes`
@@ -79,8 +110,18 @@ CREATE TABLE `votes` (
   `userId` varchar(16) NOT NULL,
   `timestamp` datetime NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `votes`
+--
+
+LOCK TABLES `votes` WRITE;
+/*!40000 ALTER TABLE `votes` DISABLE KEYS */;
+INSERT INTO `votes` VALUES (1,1,1,'b6QGLP1PTz5Qs','2013-04-04 12:12:02'),(2,1,2,'b6WGj4FwgCvPg','2013-04-04 12:12:02'),(3,1,1,'b6.zgIkTh2.Jg','2013-04-04 12:12:02'),(4,1,3,'b6hvIqVXtC1/U','2013-04-04 12:12:02'),(5,1,2,'b6Ch0HVI3RM66','2013-04-04 12:13:16'),(6,2,4,'acPnO7sRr7oKo','2013-04-04 12:14:18');
+/*!40000 ALTER TABLE `votes` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -91,4 +132,4 @@ CREATE TABLE `votes` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-04-02 16:47:34
+-- Dump completed on 2013-04-04 12:50:05
